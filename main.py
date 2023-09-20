7288be4dbd12ccb5ea7596cfca076f8699fc8a23
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-09-20 17:58:37 +0100

    Edited

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import discord
 from discord.ext import commands
 from secrets import randbelow
 import random
+import requests
+import json
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=['!', "$"], intents=intents)
+
+def quote_generator():
+    response = requests.get('https://zenquotes.io/api/random')
+    data = json.loads(response.content)
+    quote = data[0]['q'] + '~' + data[0]['a']
+    return quote
+
 
 
 @bot.event
@@ -21,6 +30,11 @@ async def hello(ctx):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@bot.command()
+def inspired(ctx):
+    quote = quote_generator()
+    ctx.send(f"Quote for {ctx.author.mention} is {quote}")
 
 @bot.command()
 async def random_(ctx, start: int, end: int):
