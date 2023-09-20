871a6d775464daa95b0b48b409c92d34b2a28d9c
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-09-20 18:08:32 +0100

    Updated

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix=['!', "$"], intents=intents)
 def quote_generator():
     response = requests.get('https://zenquotes.io/api/random')
     data = json.loads(response.content)
-    quote = data[0]['q'] + '~' + data[0]['a']
+    quote = data[0]['q'] + ' ~ ' + data[0]['a']
     return quote
 
 
@@ -25,6 +25,7 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
+    """Greets the User"""
     await ctx.send(f"Hello {ctx.author.mention}!")
 
 
@@ -35,14 +36,19 @@ async def ping(ctx):
 
 @bot.command()
 async def inspired(ctx):
+    """Generates a random quote for the user"""
     quote = quote_generator()
-    await ctx.send(f"Quote for {ctx.author.mention} is {quote}")
+    await ctx.send(f"Quote for {ctx.author.mention}\n{quote}")
 
 
 @bot.command()
 async def random_(ctx, start: int, end: int):
-    random_generated = random.randrange(start, end + 1)
-    await ctx.send(f"Random Number Generated is: {random_generated}!")
+    """Generates a random number from user provided range"""
+    if start is not None and end is not None:
+        random_generated = random.randrange(start, end + 1)
+        await ctx.send(f"Random generated number is: {random_generated}!")
+    elif start is None or end is None:
+        await ctx.send(f"Enter a range to generate random number!")
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
