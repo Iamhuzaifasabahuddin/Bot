3e06972e2cd8316acb2096ad7a36bc9ee07e51f5
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-09-20 18:03:48 +0100

    Fixed

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import discord
 from discord.ext import commands
@@ -11,6 +10,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=['!', "$"], intents=intents)
 
+
 def quote_generator():
     response = requests.get('https://zenquotes.io/api/random')
     data = json.loads(response.content)
@@ -18,29 +18,31 @@ def quote_generator():
     return quote
 
 
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
 
 @bot.command()
 async def hello(ctx):
     await ctx.send(f"Hello {ctx.author.mention}!")
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+
 @bot.command()
-def inspired(ctx):
+async def inspired(ctx):
     quote = quote_generator()
-    ctx.send(f"Quote for {ctx.author.mention} is {quote}")
+    await ctx.send(f"Quote for {ctx.author.mention} is {quote}")
+
 
 @bot.command()
 async def random_(ctx, start: int, end: int):
-    random_generated = random.randrange(start, end+1)
+    random_generated = random.randrange(start, end + 1)
     await ctx.send(f"Random Number Generated is: {random_generated}!")
-
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
